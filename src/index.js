f2fd4166b89d5ba08f5b80f5260457cc50951667
Ilya Udovichenko <rios111@yandex.ru>
2021-08-23 12:17:26 +0300

    lesson 109

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello World');
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+
+    case 'DEC':
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+// По нажатию на кнопку мы будем диспатчить событие, но пока мы ничего не видим, потому что
+// мы ничего не делаем, когда store обновляется.
+document
+  .getElementById('inc')
+  .addEventListener('click', () => store.dispatch({ type: 'INC' }));
+document
+  .getElementById('dec')
+  .addEventListener('click', () => store.dispatch({ type: 'DEC' }));
+
+// Теперь каждый раз, когда store обновляется, он будет вызывать функцию update. Ну а
+// функция update в свою очередь будет вызывать обновление нашего счетчика.
+const update = () => {
+  document.getElementById('counter').innerHTML = store.getState();
+};
+store.subscribe(update);
